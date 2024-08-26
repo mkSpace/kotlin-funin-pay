3970720697400450faae31298f3e6722b9b3609a
Jaemin.Park <once29@naver.com>
2024-08-26 23:11:32 +0900

    Initialize Project

---
+++ src/main/kotlin/com/funin/pay/KotlinFuninPayApplication.kt
@@ -0,0 +1,11 @@
+package com.funin.pay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinFuninPayApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinFuninPayApplication>(*args)
+}
---
+++ src/test/kotlin/com/funin/pay/KotlinFuninPayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.funin.pay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinFuninPayApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
